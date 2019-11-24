44b79450f4b373cf87110f98507201ae00f1016b
Oleg Sorokin <oleg.sorokin@rtbid.ru>
2019-11-24 18:06:24 +0600

    Added the rules for the ESlint

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    path: path.join(__dirname, `public`),
+    filename: `bundle.js`
+  },
+  devtool: `source-map`
+};
