4f715a22e9a45b5251555dd4f034360a2bb3aa21
Oleg Sorokin <oleg.sorokin@rtbid.ru>
2019-12-22 19:01:06 +0600

    6.1.1: Added the task controller

---
+++ src/controllers/task.js
@@ -0,0 +1,89 @@
+import {render, RenderPosition, replace} from '../utils/render';
+import TaskComponent from '../components/task';
+import TaskEditComponent from '../components/task-edit';
+
+const Mode = {
+  DEFAULT: `default`,
+  EDIT: `edit`
+};
+
+export default class TaskController {
+  constructor(container, onDataChange, onViewChange) {
+    this._container = container;
+    this._onDataChange = onDataChange;
+    this._onViewChange = onViewChange;
+
+    this._mode = Mode.DEFAULT;
+
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._onEscapeKeyDown = this._onEscapeKeyDown.bind(this);
+  }
+
+  render(task) {
+    const oldTaskComponent = this._taskComponent;
+    const oldTaskEditComponent = this._taskEditComponent;
+
+    this._taskComponent = new TaskComponent(task);
+    this._taskEditComponent = new TaskEditComponent(task);
+
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscapeKeyDown);
+    });
+
+    this._taskComponent.setArchiveButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
+    });
+
+    this._taskComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite,
+      }));
+    });
+
+    this._taskEditComponent.setSubmitHandler(() => {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscapeKeyDown);
+    });
+
+    if (oldTaskEditComponent && oldTaskComponent) {
+      replace(this._taskComponent, oldTaskComponent);
+      replace(this._taskEditComponent, oldTaskEditComponent);
+    } else {
+      render(this._container, this._taskComponent, RenderPosition.BEFOREEND);
+    }
+  }
+
+  setDefaultView() {
+    if (this._mode !== Mode.DEFAULT) {
+      this._replaceEditToTask();
+    }
+  }
+
+  _replaceEditToTask() {
+    this._taskEditComponent.reset();
+
+    replace(this._taskComponent, this._taskEditComponent);
+    this._mode = Mode.DEFAULT;
+  }
+
+  _replaceTaskToEdit() {
+    this._onViewChange();
+
+    replace(this._taskEditComponent, this._taskComponent);
+    this._mode = Mode.EDIT;
+  }
+
+  _onEscapeKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscapeKeyDown);
+    }
+  }
+}
